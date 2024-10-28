6ddeee9fa77008ab3e4dd206b46c45eb0011875a
Adam Lyu <flyu008@uottawa.ca>
2024-10-28 09:42:08 -0400

    first commit

---
+++ utils/graph.py
@@ -0,0 +1,31 @@
+"""
+@Time ： 2024-10-28
+@Auth ： Adam Lyu
+"""
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, StateGraph, START
+from langgraph.prebuilt import tools_condition
+
+# from assistant import Assistant, part_1_assistant_runnable, part_1_tools
+from chatgpt_assistant import Assistant, part_1_assistant_runnable, part_1_tools
+from state import State
+from utils.utilities import create_tool_node_with_fallback
+
+builder = StateGraph(State)
+
+
+# Define nodes: these do the work
+builder.add_node("assistant", Assistant(part_1_assistant_runnable))
+builder.add_node("tools", create_tool_node_with_fallback(part_1_tools))
+# Define edges: these determine how the control flow moves
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+
+# The checkpointer lets the graph persist its state
+# this is a complete memory for the entire graph.
+memory = MemorySaver()
+part_1_graph = builder.compile(checkpointer=memory)
